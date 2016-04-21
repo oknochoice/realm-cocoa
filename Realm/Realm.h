80526462aa3a0357b1f4a9d8f6fe044b28b2a16f
jiwei.wang <jiwei.wang@niu.com>
2016-04-21 14:08:37 +0800

    anotation

--- Realm/Realm.h
+++ Realm/Realm.h
@@ -22,7 +22,7 @@
 #import <Realm/RLMMigration.h>
 #import <Realm/RLMObject.h>
 #import <Realm/RLMObjectSchema.h>
-#import <Realm/RLMPlatform.h>
+//#import <Realm/RLMPlatform.h>
 #import <Realm/RLMProperty.h>
 #import <Realm/RLMRealm.h>
 #import <Realm/RLMRealmConfiguration.h>
